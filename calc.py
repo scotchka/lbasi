5667351b7e6b4a3cf30ebf23a42a7e4df8acb9ae
Henry Chen <tahafut@gmail.com>
2017-09-24 15:45:02 -0700

    AST classes; better repr

--- calc.py
+++ calc.py
@@ -29,6 +29,32 @@ SYMBOLS = {
 }
 
 
+class AST(object):
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+    def __repr__(self):
+
+        return 'BinOp({left}, {token}, {right})'.format(left=repr(self.left),
+                                                        token=repr(self.token.value),
+                                                        right=repr(self.right))
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+    def __repr__(self):
+        return 'Num({})'.format(self.value)
+
+
 class Token(object):
     def __init__(self, type_, value):
         self.type = type_
@@ -89,7 +115,7 @@ class Lexer(object):
         return Token(EOF, None)
 
 
-class Interpreter(object):
+class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
@@ -112,27 +138,25 @@ class Interpreter(object):
         token = self.current_token
         if token.type == INTEGER:
             self.eat(INTEGER)
-            return token.value
+            return Num(token)
         elif token.type == LPARENS:
             self.eat(LPARENS)
-            result = self.expr()
+            node = self.expr()
             self.eat(RPARENS)
-            return result
+            return node
         else:
             self.error()
 
     def term(self):
 
-        result = self.factor()
+        node = self.factor()
 
         while self.current_token.type in (MULTIPLY, DIVIDE):
-            op = self.current_token
-            self.eat(op.type)
-            right = self.factor()
-            result = OPS[op.type](result, right)
+            token = self.current_token
+            self.eat(token.type)
+            node = BinOp(left=node, op=token, right=self.factor())
 
-        return result
-
+        return node
 
     def expr(self):
         """
@@ -140,18 +164,19 @@ class Interpreter(object):
 
         """
 
-        result = self.term()
+        node = self.term()
 
         while self.current_token.type in (PLUS, MINUS):
-            op = self.current_token
+            token = self.current_token
 
-            self.eat(op.type)
+            self.eat(token.type)
 
-            right = self.term()
+            node = BinOp(left=node, op=token, right=self.term())
 
-            result = OPS[op.type](result, right)
+        return node
 
-        return result
+    def parse(self):
+        return self.expr()
 
 
 if __name__ == '__main__':
@@ -166,10 +191,10 @@ if __name__ == '__main__':
         lexer = Lexer(text)
 
         try:
-            interpreter = Interpreter(lexer)
+            parser = Parser(lexer)
         except Exception as e:
             print 'Invalid expression'
             continue
 
-        result = interpreter.expr()
+        result = parser.expr()
         print result
